a3b268e2e635e1dc5ee63ce86c27ce3738b1b3b2
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-09 11:50:41 +0000

    [Add]投稿数のグラフ化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    
+    #過去７日分それぞれの投稿数を一覧表示する
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def index
