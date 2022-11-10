b7724ec9cc6f1887fbfdb5681e7c55d3df5a5f07
nksnkmnkc <114841822+nksnkmnkc@users.noreply.github.com>
2022-11-10 11:32:58 +0000

    [Finish]8b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
-    
-    #過去７日分それぞれの投稿数を一覧表示する
+    #過去７日分それぞれの投稿数を一覧表示する（表・グラフ）
     @today_book =  @books.created_today
     @yesterday_book = @books.created_yesterday
-    @this_week_book = @books.created_this_week
-    @last_week_book = @books.created_last_week
+    @two_day_ago_book = @books.created_two_day_ago
+    @three_day_ago_book = @books.created_three_day_ago
+    @four_day_ago_book = @books.created_four_day_ago
+    @five_day_ago_book = @books.created_five_day_ago
+    @six_day_ago_book = @books.created_six_day_ago
   end
 
   def index
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
-import "popper.js"
-import "bootstrap"
+import Rails from "@rails/ujs";
+//import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "jquery";
+import "popper.js";
+import "bootstrap";
+//グラフの導入↓
+import Chart from 'chart.js/auto';
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+window.$ = window.jQuery = require('jquery');
+global.Chart = Chart;
+
+Rails.start();
+//Turbolinks.start();
+ActiveStorage.start();
+
